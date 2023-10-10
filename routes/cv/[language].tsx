c06f1783085ef02ccc3ab3fd5ac8a1feec720228
Ravi Helon de Melo Serafim Ferreira <ravihelon@gmail.com>
2023-10-10 14:05:32 -0300

    Update [language].tsx

--- routes/cv/[language].tsx
+++ routes/cv/[language].tsx
@@ -1,5 +1,5 @@
 import { PageProps, Handlers } from "$fresh/server.ts";
-import { Tcc } from './tcc';
+import { Tcc } from './tcc.tsx';
 
 export default function GreetPage(props: PageProps) {
   const { language } = props.params;
