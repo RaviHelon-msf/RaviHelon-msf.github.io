217509408e362752bf480c0a3dcb7d7ef7bb50ad
RaviHelon-msf <ravihelon@gmail.com>
2023-10-11 12:40:46 -0300

    Feat: Add generalization
    Template code is fully generalized. Any new entrance can be decoded from an Json file. Only missing JSON interface now.

--- routes/cv/[language].tsx
+++ routes/cv/[language].tsx
@@ -6,13 +6,22 @@ import Entry, {Role} from './entry.tsx';
 export default function GreetPage(props: PageProps) {
   const tcc: Role = {
     name: 'Engenheiro de dados - Estagiário',
-    at: 'Universidade Federal de Campina Grande/LIEC',
+    at: 'Universidade Federal de Campina Grande/LIEC & Petrobrás',
     description: 'Sistema para manutenção preditiva para motores elétricos a partir de dados de vibração utilizando técnicas de aprendizado de máquina.',
-    tags: ['Data' , 'Python'],
-    atrib: 
-        ['Desenvolvimento de plataforma de testes', 
-        'Design dos experimentos'],
+    tags: ['Data' , 'Python','ML','ETL','Power BI'],
+    atrib: ['Desenvolvimento de plataforma de testes', 
+        'Design dos experimentos',
+        'Coleta e tratamento de dados',
+        'Escolha e avaliação de modelos para aprendizado de máquina',
+        'Determinação de hiper-parâmetros para os modelos',
+        'Fine-tuning dos modelos',
+        'Avaliação da performance dos modelos',
+        'Interface utilizando Power BI de forma a orientar usuários de acordo com saúde do equipamento',
+        'Apresentação, inclusive em contexto científico, dos resultado obtido'],
+    sDate: 'may. 2017', 
+    eDate: 'jan. 2020',
   }
+  
   const { language } = props.params;
 
   return (
--- routes/cv/entry.tsx
+++ routes/cv/entry.tsx
@@ -4,6 +4,8 @@ export interface Role {
     description?:string;
     tags?: string[];
     atrib?: string[];
+    sDate?: string;
+    eDate?: string;
 }
 
 
@@ -15,6 +17,8 @@ const tcc: Role = {
     atrib: 
         ['TemplateAtrib1', 
         'TemplateAtrib2'],
+        sDate: 'Mon.YEAR',
+        eDate: 'Mon.YEAR',
 }
 
 export default function Entry(role: Role = tcc) {
@@ -25,47 +29,24 @@ export default function Entry(role: Role = tcc) {
           <div class="">
             <h1 class="font-outline flex items-center justify-center p-2 text-3xl font-extrabold text-[#008F11]">{role.name}</h1>
             <p class="text.outline py-1 text-[#008f11]"><span class="text-[#003B00]">@</span> {role.at}</p>
-            <p class="text.outline py-1 text-[#008f11]"><span class="text-[#003B00]">@</span> Petrobrás</p>
             <h2 class="py-1 text-base font-extralight text-[#00FF41]">{role.description}</h2>
             <div class="flex w-full py-1 justify-between">
                 <ul class="list-disc pl-4 text-sm font-extralight text-[#00FF41]">
-                    <li>
-                    <p>Desenvolvimento de plataforma de testes</p>
+                {role.atrib?.map((item,index)=>(
+                    <li key={index}>
+                        <p>{item}</p>
                     </li>
-                    <li>
-                    <p>Design dos experimentos</p>
-                    </li>
-                    <li>
-                    <p>Coleta e tratamento de dados</p>
-                    </li>
-                    <li>
-                    <p>Escolha e avaliação de modelos para aprendizado de máquina</p>
-                    </li>
-                    <li>
-                    <p>Determinação de hiper-parâmetros para os modelos</p>
-                    </li>
-                    <li>
-                    <p>Fine-tuning dos modelos</p>
-                    </li>
-                    <li>
-                    <p>Avaliação da performance dos modelos</p>
-                    </li>
-                    <li>
-                    <p>Interface utilizando Power BI de forma a orientar usuários de acordo com saúde do equipamento</p>
-                    </li>
-                    <li>
-                    <p>Apresentação, inclusive em contexto científico, dos resultado obtido</p>
-                    </li>
+                ))}
                 </ul>
                 <ul class="w-[15%] text-left text-[#00FF41]">
-                    <li><button class="hover:animate-pulse">Data</button></li>
-                    <li><button class="hover:animate-pulse">Python</button></li>
-                    <li><button class="hover:animate-pulse">ML</button></li>
-                    <li><button class="hover:animate-pulse">ETL</button></li>
-                    <li><button class="hover:animate-pulse">Power BI</button></li>
+                {role.tags?.map((item,index)=>(
+                    <li key={index}>
+                        <button class = "hover:animate-pulse">{item}</button>
+                    </li>
+                ))}
                 </ul>
                 </div>
-            <div class="flex justify-between px-[25%] py-1 text-center text-xs text-[#00FF41]"><span>may. 2017</span><span> jan. 2020</span></div>
+            <div class="flex justify-between px-[25%] py-1 text-center text-xs text-[#00FF41]"><span>{role.sDate}</span><span>{role.eDate}</span></div>
           </div>
         </>
     )
