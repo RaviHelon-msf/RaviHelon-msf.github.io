bcbd5658301d9603a7c2b9d3d0bcaab5041c68ff
RaviHelon-msf <ravihelon@gmail.com>
2023-10-30 11:47:34 -0300

    Feat: Add basic contact info
    Added my social network links for basic contacting, as well as layout and styling. Also added svgs for each social

--- routes/contact/index.tsx
+++ routes/contact/index.tsx
@@ -38,14 +38,28 @@ export default async function About(_req: Request) {
   return(
     <>
       <MyHeader />
+      <section class="flex flex-col text-green-500 px-[10%] py-[3%] gap-4">
+        <h1 class = "font-bold text-xl justify-center flex"> Redes sociais </h1>
+        <a href="https://www.github.com/RaviHelon-msf" class=" flex flex-row items-center">
+            <img src="/contact/github.svg" alt="github" class = "w-[50px] h-[50px]" />
+            <p> RaviHelon-msf </p>
+        </a>
 
-      <div>
-          <h1>{name}</h1>
-          <p>{url}</p>
-          <p>{email}</p>
-          <p>{bio}</p>
-          <b>{ hireable ? "Disponível para novos projetos" : "Ocupado para novos projetos."}</b>
-      </div>
+        <a href="https://www.linkedin.com/in/ravi-helon" class=" flex flex-row items-center">
+            <img src="/contact/linkedin.svg" alt="linkedin" class = "w-[50px] h-[50px]" />
+            <p> /in/ravi-helon</p>
+        </a>
+
+        <a href="mailto:ravihelon@gmail.com" class=" flex flex-row items-center">
+            <img src="/contact/gmail.svg" alt="gmail" class = "w-[50px] h-[50px]" />
+            <p> ravihelon@gmail.com </p>
+        </a>
+
+        <a href="tel:+5583986695155" class=" flex flex-row items-center">
+            <img src="/contact/whatsapp-logo.svg" alt="whatsapp" class = "w-[50px] h-[50px]" />
+            <p> +55 (83) 9 8669-5155 </p>
+        </a>
+      </section>
     </>
   );
 }
