245ea7107389f42358032b53c684ec651211a0be
RaviHelon-msf <ravihelon@gmail.com>
2023-11-12 12:54:53 -0300

    Feat: Add Project input
    Add the project input with interchagible interfaces as well as basic sanitizing for the form submission

--- islands/contact/About.tsx
+++ islands/contact/About.tsx
@@ -2,97 +2,43 @@ import { useState } from "preact/hooks";
 
 export function ProjectForm() {
   const [projectName, setProjectName] = useState("");
-  const [projectScope, setProjectScope] = useState("");
-  const [budget, setBudget] = useState("");
   const [parties, setParties] = useState(["Yourself"]);
+  const [submitted, setSubmitted] = useState(false);
 
-  const handleProjectNameChange = (e) => {
-    const value = e.target.value;
-    setProjectName(value);
+  const submitProject = (e) => {
+    e.preventDefault();
+    setSubmitted(true);
+  }
 
-    // Update HTML or perform other actions with the value
-    updateHtml("projectName", value);
-  };
-
-  const handleProjectScopeChange = (e) => {
-    const value = e.target.value;
-    setProjectScope(value);
-
-    // Update HTML or perform other actions with the value
-    updateHtml("projectScope", value);
-  };
-
-  const handleBudgetChange = (e) => {
-    const value = e.target.value;
-    setBudget(value);
-
-    // Update HTML or perform other actions with the value
-    updateHtml("budget", value);
-  };
-
-  const updateHtml = (field, value) => {
-    // Perform actions to dynamically update HTML based on the field and value
-    console.log(`Update HTML for ${field}: ${value}`);
-  };
-
-  const addParty = () => {
-    const newParty = "New Party";
-    setParties((prevParties) => [...prevParties, newParty]);
-
-    // Update HTML or perform other actions with the new party
-    updateHtml("party", newParty);
-  };
+  const handleInput = (e) => {
+    const sanitizedInput = e.target.value.replace(/[^a-zA-Z\s]/g, "");
+    setProjectName(sanitizedInput);
+  }
 
   return (
-    <main class="flex flex-row h-[1100px] w-full px-[10%] py-[3%] text-[#008F11]">
-      <form>
-        <label>
-          Project Name:
-          <input
-            type="text"
-            value={projectName}
-            onInput={handleProjectNameChange}
-          />
-        </label>
-      </form>
-      {projectName && (
-        <div>
-          <p>Project Name: {projectName}</p>
-        </div>
+    <main class="pt-7 flex flex-col gap-4 w-full min-h-[150px] px-[10%] py-[3%] text-[#10b981] items-start justify-center">
+      {(!submitted)?(
+          <>
+            <h1 class="w-full font-bold text-center text-3xl h-[50px]" > 
+              Quer trabalhar comigo? 
+            </h1>
+            <form onSubmit={submitProject}>
+              <label>
+                Project Name:
+                <input
+                  type="text"
+                  value={projectName}
+                  onInput={handleInput}
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </form>
+          </>
+      ) : (
+        <>
+          <h1 class="w-full font-bold text-center text-3xl h-[50px]" > Project: { projectName } </h1>
+        </>
       )}
-      <form>
-        <label>
-          Project Scope:
-          <textarea
-            value={projectScope}
-            onInput={handleProjectScopeChange}
-          ></textarea>
-        </label>
-      </form>
-      {projectScope && (
-        <div>
-          <p>Project Scope: {projectScope}</p>
-        </div>
-      )}
-
-      <form>
-        <label>
-          Budget:
-          <input type="text" value={budget} onInput={handleBudgetChange} />
-        </label>
-      </form>
-      {budget && (
-        <div>
-          <p>Budget: {budget}</p>
-        </div>
-      )}
-
-      <button onClick={addParty}>Add Party</button>
-      {parties.map((party, index) => (
-        <div key={index}>
-          <p>Party {index + 1}: {party}</p>
-        </div>
-      ))}
     </main>
   );
 }
