97a2c187fe498e376c9b9cf94aa871989b62e237
RaviHelon-msf <ravihelon@gmail.com>
2023-11-11 22:17:51 -0300

    Feat:Add Proj Page
    Add project section to about page

--- islands/contact/About.tsx
+++ islands/contact/About.tsx
@@ -1,48 +1,100 @@
 import { useState } from "preact/hooks";
 
-
 export function ProjectForm() {
-    const [projectName, setProjectName] = useState('');
-    const [projectScope, setProjectScope] = useState('');
-    const [budget, setBudget] = useState('');
-    const [submitted, setSubmitted] = useState(false);
+  const [projectName, setProjectName] = useState("");
+  const [projectScope, setProjectScope] = useState("");
+  const [budget, setBudget] = useState("");
+  const [parties, setParties] = useState(["Yourself"]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const formEvent = new CustomEvent("submitForm", {
-          detail: { projectName, projectScope, budget },
-        });
-        document.dispatchEvent(formEvent);
-        setSubmitted(true);
-      };
+  const handleProjectNameChange = (e) => {
+    const value = e.target.value;
+    setProjectName(value);
 
-    return (
-            <main class="flex flex-row h-[1100px] w-full px-[10%] py-[3%] text-[#008F11]">
-            <form onSubmit={handleSubmit}>
-            <label>
-                Project Name:
-                <input type="text" value={projectName} onInput={(e) => setProjectName((e.target as HTMLInputElement).value)} required />
-            </label>
-            {submitted && (
-                <>
-                <label>
-                    Project Scope:
-                    <textarea value={projectScope} onInput={(e) => setProjectScope((e.target as HTMLTextAreaElement).value)}></textarea>
-                </label>
-                <label>
-                    Budget:
-                    <input type="text" value={budget} onInput={(e) => setBudget((e.target as HTMLInputElement).value)} />
-                </label>
-                <label>
-                    Parties:
-                    <input type="text" value="Yourself" readOnly />
-                </label>
-                </>
-            )}
-            <button type="submit">Submit</button>
-            </form>
-        </main>
-    );
-};
+    // Update HTML or perform other actions with the value
+    updateHtml("projectName", value);
+  };
+
+  const handleProjectScopeChange = (e) => {
+    const value = e.target.value;
+    setProjectScope(value);
+
+    // Update HTML or perform other actions with the value
+    updateHtml("projectScope", value);
+  };
+
+  const handleBudgetChange = (e) => {
+    const value = e.target.value;
+    setBudget(value);
+
+    // Update HTML or perform other actions with the value
+    updateHtml("budget", value);
+  };
+
+  const updateHtml = (field, value) => {
+    // Perform actions to dynamically update HTML based on the field and value
+    console.log(`Update HTML for ${field}: ${value}`);
+  };
+
+  const addParty = () => {
+    const newParty = "New Party";
+    setParties((prevParties) => [...prevParties, newParty]);
+
+    // Update HTML or perform other actions with the new party
+    updateHtml("party", newParty);
+  };
+
+  return (
+    <main class="flex flex-row h-[1100px] w-full px-[10%] py-[3%] text-[#008F11]">
+      <form>
+        <label>
+          Project Name:
+          <input
+            type="text"
+            value={projectName}
+            onInput={handleProjectNameChange}
+          />
+        </label>
+      </form>
+      {projectName && (
+        <div>
+          <p>Project Name: {projectName}</p>
+        </div>
+      )}
+      <form>
+        <label>
+          Project Scope:
+          <textarea
+            value={projectScope}
+            onInput={handleProjectScopeChange}
+          ></textarea>
+        </label>
+      </form>
+      {projectScope && (
+        <div>
+          <p>Project Scope: {projectScope}</p>
+        </div>
+      )}
+
+      <form>
+        <label>
+          Budget:
+          <input type="text" value={budget} onInput={handleBudgetChange} />
+        </label>
+      </form>
+      {budget && (
+        <div>
+          <p>Budget: {budget}</p>
+        </div>
+      )}
+
+      <button onClick={addParty}>Add Party</button>
+      {parties.map((party, index) => (
+        <div key={index}>
+          <p>Party {index + 1}: {party}</p>
+        </div>
+      ))}
+    </main>
+  );
+}
 
 export default ProjectForm;
