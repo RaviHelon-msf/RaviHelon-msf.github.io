c2827f8f784d609feb4f5df7ce07a9db7a7cc39f
RaviHelon-msf <ravihelon@gmail.com>
2023-11-14 14:44:53 -0300

    Feat: Add Footer
    Add a footer with all the social media links and other site references

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,13 +5,12 @@
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/_middleware.ts";
-import * as $3 from "./routes/contact/[name].tsx";
-import * as $4 from "./routes/contact/index.tsx";
-import * as $5 from "./routes/cv/index.tsx";
-import * as $6 from "./routes/index.tsx";
-import * as $7 from "./routes/projects/index.tsx";
-import * as $8 from "./routes/skills/index.tsx";
-import * as $9 from "./routes/test.tsx";
+import * as $3 from "./routes/contact/index.tsx";
+import * as $4 from "./routes/cv/index.tsx";
+import * as $5 from "./routes/index.tsx";
+import * as $6 from "./routes/projects/index.tsx";
+import * as $7 from "./routes/skills/index.tsx";
+import * as $8 from "./routes/test.tsx";
 import * as $$0 from "./islands/Cv.tsx";
 import * as $$1 from "./islands/contact/About.tsx";
 
@@ -20,13 +19,12 @@ const manifest = {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
     "./routes/_middleware.ts": $2,
-    "./routes/contact/[name].tsx": $3,
-    "./routes/contact/index.tsx": $4,
-    "./routes/cv/index.tsx": $5,
-    "./routes/index.tsx": $6,
-    "./routes/projects/index.tsx": $7,
-    "./routes/skills/index.tsx": $8,
-    "./routes/test.tsx": $9,
+    "./routes/contact/index.tsx": $3,
+    "./routes/cv/index.tsx": $4,
+    "./routes/index.tsx": $5,
+    "./routes/projects/index.tsx": $6,
+    "./routes/skills/index.tsx": $7,
+    "./routes/test.tsx": $8,
   },
   islands: {
     "./islands/Cv.tsx": $$0,
